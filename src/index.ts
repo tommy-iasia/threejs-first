c15d9e767669b37a913ffe12c6851ba4a6687fd5
tommylee <tommylee@iasia.com.hk>
2021-11-12 18:16:50 +0800

    basic scene

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 start();
 
@@ -8,16 +9,23 @@ export function start() {
 
   const scene = new THREE.Scene();
 
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  scene.add(directionalLight);
+
   const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-  const material = new THREE.MeshNormalMaterial();
+  const material = new THREE.MeshPhongMaterial({ color: 0xb504f4 });
 
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
 
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   document.body.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.25;
 
   renderer.setAnimationLoop((time) => {
     mesh.rotation.x = time / 2000;
